1a3ae210f1d0c913c2232d03bb2422265726fc91
Liddeldiiz <s218711@student.uek.krakow.pl>
2022-09-28 23:34:14 +0200

    alternating LED on GPIO5 of the esp8266

---
+++ ConceptLED/ConceptLED.ino
@@ -0,0 +1,16 @@
+#define ledpin 5
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledpin, HIGH);
+
+  delay(1000);
+
+  digitalWrite(ledpin, LOW);
+
+  delay(1000);
+}
