37777468cfdc356ee4bd9ad310d9674a69986a44
Liddeldiiz <s218711@student.uek.krakow.pl>
2022-10-01 17:48:25 +0200

    simple HelloWorld on NodeJS server

--- ConceptLED/ConceptLED.ino
+++ ConceptLED/ConceptLED.ino
@@ -1,28 +1,16 @@
 #define ledpin 5
-#define buttonpin 16
 
-int temp = 0;
-
-int switchLedState(int temp) {
-  if(temp == 0) {
-    digitalWrite(ledpin, HIGH);
-    temp = 1;
-    delay(1000);
-  } else {
-    digitalWrite(ledpin, LOW);
-    temp = 0;
-    delay(1000);
-  }
-  return temp;
-}
 
 void setup() {
   pinMode(ledpin, OUTPUT);
-  pinMode(buttonpin, INPUT);
 }
 
 void loop() {
-  if(digitalRead(buttonpin) == HIGH) {
-    temp = switchLedState(temp);
-  }
+  digitalWrite(ledpin, HIGH);
+
+  delay(1000);
+
+  digitalWrite(ledpin, LOW);
+
+  delay(1000);
 }
